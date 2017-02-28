1ed985213f3939c8d928aabd1e7e1617c8394df4
Agung Nugraha <nugraha.agung52@gmail.com>
2017-02-28 15:51:50 +0700

    Add files via upload

---
+++ Minggu Ke-1/HelloWorld.java
@@ -0,0 +1,6 @@
+/*Program Helloword*/
+class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		}
+}
---
+++ Minggu Ke-1/HelloWorld_Netbean/HelloWorld/src/HelloWorld/java/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HelloWorld.java;
+
+/**
+ *
+ * @author Fays
+ */
+ /*Program Helloword*/
+public class HelloWorld {
+    	public static void main(String argv[]) {
+		System.out.println("Hello World");
+		}
+    
+}
