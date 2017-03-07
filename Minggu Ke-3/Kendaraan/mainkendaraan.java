a5ae1463b4e9f80959a57c603f67e62ecb4b5d5e
Agung Nugraha <nugraha.agung52@gmail.com>
2017-03-07 22:58:35 +0700

    Add files via upload

---
+++ Minggu Ke-3/Kendaraan/mainkendaraan.java
@@ -0,0 +1,9 @@
+public class mainkendaraan
+{
+     public static void main(String[]args)
+	 {
+		 mobil agung = new mobil();
+	     agung.merk_mobil = "Lamborghini";
+		 agung.maju_mobil();
+	 }
+}
---
+++ Minggu Ke-3/Kendaraan/mobil.java
@@ -0,0 +1,19 @@
+class mobil
+{
+   String merk_mobil;
+   int harga_mobil,berat_mobil,kapasitas_mobil;
+   
+	void maju_mobil()
+	{
+		System.out.println("Mobil anda melaju dengan kecepatan 200 KM per jam...");	
+	}
+	void mundur_mobil()
+	{
+		
+	}
+	void berhenti_mobil()
+	{
+		
+	}
+      
+}
