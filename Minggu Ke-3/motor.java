b0ac32162f9025286f213f3085551f376f652b5a
Agung Nugraha <nugraha.agung52@gmail.com>
2017-03-07 22:45:27 +0700

    Add files via upload

---
+++ Minggu Ke-3/motor.java
@@ -0,0 +1,21 @@
+class motor
+{
+	String merk_motor,tahun_produksi;
+	int harga_motor,berat_motor;
+    void tampungan()
+	{
+		
+	}
+	void maju()
+	{
+		
+	}
+	void mundur()
+	{
+		
+	}
+	void berhenti()
+	{
+		
+	}
+}
