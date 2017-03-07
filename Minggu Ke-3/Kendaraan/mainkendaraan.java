44f5b7167c505e81729d6736070485e90ab49bbe
Agung Nugraha <nugraha.agung52@gmail.com>
2017-03-07 23:02:07 +0700

    Delete mainkendaraan.java

--- Minggu Ke-3/Kendaraan/mainkendaraan.java
+++
@@ -1,9 +0,0 @@
-public class mainkendaraan
-{
-     public static void main(String[]args)
-	 {
-		 mobil agung = new mobil();
-	     agung.merk_mobil = "Lamborghini";
-		 agung.maju_mobil();
-	 }
-}
