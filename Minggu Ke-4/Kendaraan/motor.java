6fe1dda9233a0b4218b7a1691923997ef979acb9
Agung Nugraha <nugraha.agung52@gmail.com>
2017-03-14 23:47:19 +0700

    Add files via upload

---
+++ Minggu Ke-4/Kendaraan/motor.java
@@ -0,0 +1,13 @@
+class motor
+{
+	int harga_motor;
+    int tahun_produksi;
+	String merk_motor;
+    
+	public void data_motor(int harga_motor, int tahun_produksi, String merk_motor)
+	{
+	   this.harga_motor = harga_motor;
+	   this.tahun_produksi = tahun_produksi;
+	   this.merk_motor = merk_motor;
+	}
+}
