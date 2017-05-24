b313b6c8b70a019f522fa645c68b3d5c206a4f7f
Agung Nugraha <nugraha.agung52@gmail.com>
2017-05-24 22:33:05 +0700

    interface

---
+++ Minggu Ke-11/MainKendaraan.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class MainKendaraan{
+         	
+	public static void main(String argd[]){
+                char menu;
+                motor mtr = new motor();
+                mobil mbl = new mobil();
+                
+                System.out.println("+++++Program Kendaraan+++++");
+                System.out.println("A. Rincian Pembeli Motor");
+                System.out.println("B. Rincian Pembeli Mobil");
+                
+                Scanner input = new Scanner (System.in);
+                System.out.print("Masukan Menu = ");
+                menu = input.next().charAt(0);
+                
+                switch (menu){
+                    case 'A': 
+                              mtr.setNama(input.nextLine());
+                              System.out.print("Nama = ");
+                              mtr.setNama(input.nextLine());
+                              System.out.print("Merk = ");
+                              mtr.setMerk(input.nextLine());
+                              System.out.print("Thn Produksi = ");
+                              mtr.setThn_Produksi(input.nextLine());
+                              System.out.print("Harga =");
+                              mtr.setHarga(input.nextInt());
+							  System.out.print("Berat =");
+                              mtr.setBerat(input.nextInt());
+                              
+                              mtr.infokendaraan();
+                              mtr.rinciankendaraan();
+                              mtr.rincianpembeli();
+                              mtr.garispenutup();
+                              break;
+                
+                    case 'B': 
+					          mtr.setNama(input.nextLine());
+                              System.out.print("Nama = ");
+                              mtr.setNama(input.nextLine());
+                              System.out.print("Merk = ");
+                              mtr.setMerk(input.nextLine());
+                              System.out.print("Thn Produksi = ");
+                              mtr.setThn_Produksi(input.nextLine());
+                              System.out.print("Harga =");
+                              mtr.setHarga(input.nextInt());
+							  System.out.print("Berat =");
+                              mtr.setBerat(input.nextInt());
+                              
+                              mtr.infokendaraan();
+                              mtr.rinciankendaraan();
+                              mtr.rincianpembeli();
+                              mtr.garispenutup();
+                              break;
+                }
+	}
+}
---
+++ Minggu Ke-11/macam_kendaraan.java
@@ -0,0 +1,53 @@
+public abstract class macam_kendaraan implements interfaceKendaraan{
+	String Merk,Thn_Produksi,Nama;
+	int Harga,Berat;
+	
+	
+	//set and get
+	
+	public String getMerk(){
+        return Merk;
+    }
+   
+    public void setMerk(String Merk){
+        this.Merk = Merk;
+    }
+
+    public String getThn_Produksi(){
+        return Thn_Produksi;
+    }
+    
+    public void setThn_Produksi(String Thn_Produksi){
+        this.Thn_Produksi = Thn_Produksi;
+    }
+    
+    public String getNama(){
+        return Nama;
+    }
+    
+    public void setNama(String Nama){
+        this.Nama = Nama;
+    }
+    public int getHarga(){
+        return Harga;
+    }
+    
+    public void setHarga(int Harga){
+        this.Harga = Harga;
+    }
+
+    public int getBerat(){
+        return Berat;
+    }
+    
+    public void setBerat(int Berat){
+        this.Berat = Berat;
+    }
+    
+ 
+	public void Berat_Kendaraan(int j){
+		Berat=j;	
+	}
+ 
+    protected abstract void rinciankendaraan();
+}
